cb999c70a5a5293daab41e1f187a93a5cbf34d3b
ANA FLÁVIA BUENO GOMES <06006399@acad.unifeso.lan>
2024-09-19 17:05:13 -0300

    improving the return on values ​​and adding new figures

--- abstractClass/src/abstractclass/AbstractClass.java
+++ abstractClass/src/abstractclass/AbstractClass.java
@@ -6,12 +6,19 @@ public class AbstractClass {
 
     public static void main(String[] args) {
         GeometricFigure figure1 = new Circle(5.00);
-        System.out.println(figure1.getArea());
-        System.out.println(figure1.getPerimeter());
+        System.out.println("A área do círculo é de " + figure1.getArea());
+        System.out.println("O perímetro do círculo é de " + figure1.getPerimeter());
 
         GeometricFigure figure2 = new Rectangle(5.00, 10.00);
-        System.out.println(figure2.getArea());
-        System.out.println(figure2.getPerimeter());
+        System.out.println("A área do retângulo é de " + figure2.getArea());
+        System.out.println("O perímetro do retângulo é de " + figure2.getPerimeter());
+        
+        GeometricFigure figure3 = new Trapeze(5.00, 10.00, 15.00, 20.00, 25.00);
+        System.out.println("A área do trapézio é de " + figure3.getArea());
+        System.out.println("O perímetro do trapézio é de " + figure3.getPerimeter());
+        
+        GeometricFigure figure4 = new Triangle(5.00, 10.00, 15.00, 20.00);
+        System.out.println("A área do triângulo é de " + figure4.getArea());
+        System.out.println("O perímetro do triângulo é de " + figure4.getPerimeter());
     }
-    
 }
