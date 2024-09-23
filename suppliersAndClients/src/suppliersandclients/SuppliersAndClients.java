a6d2070a7c9abd0bd3ea3e7f96c8502b5c9bee4e
ANA FLÁVIA BUENO GOMES <06006399@acad.unifeso.lan>
2024-09-23 18:26:33 -0300

    initial commit

---
+++ suppliersAndClients/src/suppliersandclients/SuppliersAndClients.java
@@ -0,0 +1,11 @@
+package suppliersandclients;
+
+public class SuppliersAndClients {
+
+    public static void main(String[] args) {
+        PrincipalWindow janela = new PrincipalWindow();
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+    }
+    
+}
