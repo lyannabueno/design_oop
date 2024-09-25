1e3cc08485d998cf44a6909e59e466ffd1cc5195
ANA FLÁVIA BUENO GOMES <06006399@acad.unifeso.lan>
2024-09-25 20:56:46 -0300

    initial commit

---
+++ mercadoLivre/src/mercadolivre/MercadoLivre.java
@@ -0,0 +1,9 @@
+package mercadolivre;
+
+public class MercadoLivre {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ mercadoLivre/src/mercadolivre/entidade/pessoa.java
@@ -0,0 +1,22 @@
+package mercadolivre.entidade;
+
+public class pessoa {
+    private int id;
+    private String nome;
+    
+    public int obterId() {    
+        return id;
+    }
+    
+    public void alterarId(int id) {
+        this.id = id;
+    }
+    
+    public int obterNome() {    
+        return nome;
+    }
+    
+    public void alterarNome(String nome) {
+        this.nome = nome;
+    }
+}
