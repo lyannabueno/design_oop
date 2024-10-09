75659cdebc9fb2c6d9ba7ee98a95a227d4c0a6a9
ANA FLÁVIA BUENO GOMES <06006399@acad.unifeso.lan>
2024-10-09 20:49:32 -0300

    creating the controllers folder

---
+++ mercadoLivre/src/mercadolivre/controladores/ControladorProduto.java
@@ -0,0 +1,15 @@
+package mercadolivre.controladores;
+
+import java.util.ArrayList;
+import mercadolivre.entidade.Produto;
+
+public class ControladorProduto {
+    ArrayList<Produto> produto;
+    
+    public ControladorProduto() {
+        this.produto = new ArrayList<>
+    }
+    
+    public Produto localizar(int id) {
+    }
+}
