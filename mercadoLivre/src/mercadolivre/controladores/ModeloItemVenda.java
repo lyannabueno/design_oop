0c9a9aacea2e5934af66d62a9d7d5c9fb4a54931
ANA FLÁVIA BUENO GOMES <06006399@acad.unifeso.lan>
2024-10-16 21:10:01 -0300

    completing the item sale model class

--- mercadoLivre/src/mercadolivre/controladores/ModeloItemVenda.java
+++ mercadoLivre/src/mercadolivre/controladores/ModeloItemVenda.java
@@ -1,14 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mercadolivre.controladores;
 
-/**
- *
- * @author 06006399
- */
-public class ModeloItemVenda {
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+import mercadolivre.entidade.ItemVenda;
+
+public class ModeloItemVenda extends AbstractTableModel {
+    private ArrayList<ItemVenda> itens;
     
+    public ModeloItemVenda() {
+        itens = new ArrayList<>();
+    }
+    
+    private static final int COLUNA_NUMERO = 0;
+    private static final int COLUNA_IDPRODUTO = 1;
+    private static final int COLUNA_QUANTIDADE = 2;
+    private static final int COLUNA_PRECO = 3;
+    private static final int COLUNA_TOTAL = 4;
+    private String[] colunas = new String[]{"Número", "IdProduto", "Quantidade", "Preço", "Total"};
+
+    @Override
+    public int getColumnCount(){
+        return colunas.length;
+    }
+
+    @Override
+    public int getRowCount(){
+        return itens.size();
+    }
+
+    @Override
+    public String getColumnName(int columnIndex){
+        return colunas[columnIndex];
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex){
+        return false;
+    }
+
+    @Override
+    public Object getValueAt(int row, int col){
+        ItemVenda itemVenda = itens.get(row);
+        switch(col){
+            case COLUNA_NUMERO:
+                return itemVenda.obterNumero();
+            case COLUNA_IDPRODUTO:
+                return itemVenda.obterIdProduto();
+            case COLUNA_QUANTIDADE:
+                return itemVenda.obterQuantidade();
+            case COLUNA_PRECO:
+                return itemVenda.obterPreco();
+            case COLUNA_TOTAL:
+                return itemVenda.obterTotal();
+        }
+        return "";
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int row, int column){
+    ItemVenda itemVenda = itens.get(row);
+
+    switch(column){
+        case COLUNA_NUMERO:
+            itemVenda.alterarNumero(Integer.parseInt(aValue.toString()));
+            break;
+        case COLUNA_IDPRODUTO:
+            itemVenda.alterarProduto(aValue.toString());
+            break;
+        case COLUNA_QUANTIDADE:
+            itemVenda.alterarQuantidade(Integer.parseInt(aValue.toString()));
+            break;
+        case COLUNA_PRECO:
+            itemVenda.alterarPreco(Float.parseFloat(aValue.toString()));
+            break;
+        }
+    }
 }
