f5257cb93f938f4b415c8984beac018b5fea6a67
Lyanna Bueno <anabuenogomes@hotmail.com>
2024-11-03 21:36:47 -0300

    returning a list of rentals already made

--- aluguelPatins/src/controladores/ControladorAluguel.java
+++ aluguelPatins/src/controladores/ControladorAluguel.java
@@ -2,6 +2,7 @@ package controladores;
 
 import entidade.Aluguel;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ControladorAluguel {
     private ArrayList<Aluguel> alugueis;
@@ -16,5 +17,9 @@ public class ControladorAluguel {
     
     public void encerraAluguel(Aluguel aluguel) {
         alugueis.remove(aluguel);
+    }
+    
+    public List<Aluguel> getAlugueis() {
+        return new ArrayList<>(alugueis); // retorna uma nova lista para evitar modificações externas
     }
 }
