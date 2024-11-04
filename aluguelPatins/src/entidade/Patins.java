4d82602f80ce2c7b453993ee93913fb88d175435
Lyanna Bueno <anabuenogomes@hotmail.com>
2024-11-03 21:30:24 -0300

    adding the total value as attribute and method

--- aluguelPatins/src/entidade/Patins.java
+++ aluguelPatins/src/entidade/Patins.java
@@ -3,19 +3,21 @@ package entidade;
 public class Patins {
     private int idPatins, numeroCalcado, quantidade;
     private String estado;
+    private float valorTotal;
     
-    public Patins(int idPatins, String estado, int numeroCalcado, int quantidade) {
+    public Patins(int idPatins, String estado, int numeroCalcado, int quantidade, float valorTotal) {
         this.idPatins = idPatins;
         this.estado = estado;
         this.numeroCalcado = numeroCalcado;
         this.quantidade = quantidade;
+        this.valorTotal = valorTotal;
     }
     
     public int getIdPatins() {
         return idPatins;
     }
     
-    public void seIdPatins() {
+    public void seIdPatins(int idPatins) {
         this.idPatins = idPatins;
     }
     
@@ -42,4 +44,12 @@ public class Patins {
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
-}
+    
+    public float getValorTotal() {
+        return valorTotal;
+    }
+    
+    public void setValorTotal(float valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+ }
