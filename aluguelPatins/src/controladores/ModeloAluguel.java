e6709d94bd6c4f0a236d1f9fc44ff5d83ef0593f
Lyanna Bueno <anabuenogomes@hotmail.com>
2024-11-03 21:33:03 -0300

    Adding new columns to the table and correcting the get and set methods.

--- aluguelPatins/src/controladores/ModeloAluguel.java
+++ aluguelPatins/src/controladores/ModeloAluguel.java
@@ -1,6 +1,7 @@
 package controladores;
 
 import entidade.Aluguel;
+import entidade.Cliente;
 import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
 
@@ -13,11 +14,13 @@ public class ModeloAluguel extends AbstractTableModel {
     
     private static final int COLUNA_IDPATINS = 0;
     private static final int COLUNA_NUMEROCALCADO = 1;
-    private static final int COLUNA_IDCLIENTE = 2;
-    private static final int COLUNA_FORMAPAGAMENTO = 3;
-    private static final int COLUNA_ESTADO = 4;
+    private static final int COLUNA_ESTADO = 2;
+    private static final int COLUNA_CPF = 3;
+    private static final int COLUNA_TELEFONE = 4;
+    private static final int COLUNA_FORMAPAGAMENTO = 5;
+    private static final int COLUNA_VALORTOTAL = 6;
     
-    private String[] colunas = new String[]{"IdPatins", "NumeroCalcado", "IdCliente", "FormaPagamento", "Estado"};
+    private String[] colunas = new String[]{"IdPatins", "NumeroCalcado", "Estado", "CPF", "Telefone", "FormaPagamento", "ValorTotal"};
 
     @Override
     public int getColumnCount(){
@@ -44,35 +47,61 @@ public class ModeloAluguel extends AbstractTableModel {
         Aluguel aluguel = alugueis.get(row);
         switch(col){
             case COLUNA_IDPATINS:
-                return aluguel.getEscolhaPatins();
+                return aluguel.getEscolhaPatins().getIdPatins();
             case COLUNA_NUMEROCALCADO:
-                return aluguel.getEscolhaPatins();
-            case COLUNA_IDCLIENTE:
-                return aluguel.getIdCliente();
-            case COLUNA_FORMAPAGAMENTO:
-                return aluguel.getFormaPagamento();
+                return aluguel.getEscolhaPatins().getNumeroCalcado();
             case COLUNA_ESTADO:
                 return aluguel.getEstado();
+            case COLUNA_CPF:
+                return aluguel.getIdCliente().getCPF();
+            case COLUNA_TELEFONE:
+                return aluguel.getIdCliente().getTelefone();
+            case COLUNA_FORMAPAGAMENTO:
+                return aluguel.getFormaPagamento();
+            case COLUNA_VALORTOTAL:
+                return aluguel.getValorTotal();
         }
         return "";
     }
 
     @Override
     public void setValueAt(Object aValue, int row, int column){
-        Aluguel aluguel = alugueis.get(row);
-        Aluguel aluguelNovo = (Aluguel)aValue;
+        if (aValue instanceof Aluguel) {
 
-        switch(column){
-            case COLUNA_IDPATINS:
-                aluguel.setEscolhaPatins(aluguelNovo.getEscolhaPatins());           
-            case COLUNA_NUMEROCALCADO:
-                aluguel.setEscolhaPatins(aluguelNovo.getEscolhaPatins());            
-            case COLUNA_IDCLIENTE:
-                aluguel.setIdCliente(aluguelNovo.getIdCliente());            
-            case COLUNA_FORMAPAGAMENTO:
-                aluguel.setFormaPagamento(aluguelNovo.getFormaPagamento());            
-            case COLUNA_ESTADO:
-                aluguel.setEstado(aluguelNovo.getEstado());            
+            Aluguel aluguel = alugueis.get(row);
+            Aluguel aluguelNovo = (Aluguel)aValue;
+
+            switch (column) {
+                case COLUNA_IDPATINS:
+                    aluguel.setEscolhaPatins(aluguelNovo.getEscolhaPatins());
+                    break;
+                case COLUNA_NUMEROCALCADO:
+                    if (aluguel.getEscolhaPatins() != null) {
+                        aluguel.getEscolhaPatins().setNumeroCalcado(aluguelNovo.getEscolhaPatins().getNumeroCalcado());
+                    }
+                    break;
+                case COLUNA_ESTADO:
+                    aluguel.setEstado(aluguelNovo.getEstado());
+                    break;
+                case COLUNA_CPF:
+                    Cliente clienteNovo = aluguelNovo.getIdCliente();
+                    if (clienteNovo != null) {
+                        aluguel.setIdCliente(clienteNovo);
+                    }
+                    break;
+                case COLUNA_TELEFONE:
+                    if (aluguel.getIdCliente() != null) {
+                        aluguel.getIdCliente().setTelefone(aluguelNovo.getIdCliente().getTelefone());
+                    }
+                    break;
+                case COLUNA_FORMAPAGAMENTO:
+                    aluguel.setFormaPagamento(aluguelNovo.getFormaPagamento());
+                    break;
+                case COLUNA_VALORTOTAL:
+                    aluguel.setValorTotal(aluguelNovo.getValorTotal());
+                    break;
+            }
+            fireTableCellUpdated(row, column);
         }
     }
 }
